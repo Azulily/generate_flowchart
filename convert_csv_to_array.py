0ea44b52eae334c48a91613c513363bce3a66843
Azulily <airimatsumoto@gmail.com>
2023-04-25 18:07:45 +0900

    雑多なデータに対応する

--- convert_csv_to_array.py
+++ convert_csv_to_array.py
@@ -1,5 +1,6 @@
 import csv
-
+import re
+from datetime import datetime
 #配列宣言
 
 ENCODING = 'cp932'
@@ -10,9 +11,27 @@ MyPath = './data.csv'
 def read_csv():
     with open(MyPath,'r', encoding=ENCODING) as f:
         reader = csv.reader(f)
-        data = None
-        #csvファイルのデータをループ
+        data = []
         for row in reader:
-            next(reader)  # ヘッダー行をスキップ
-            data = [[row[1].replace("http://", "").replace("https://", ""), row[0]] for row in reader]
+            # 'http://' と 'https://' を空文字列に置換した url を生成
+            url = row[1].replace("http://", "").replace("https://", "")
+            text = re.sub('<h1.*?>|</h1>|<b>|<a.*?>|</a>|<span.*?>|</span>|【|】|！|～', '', row[4])
+            # data リストに [url, text] の形式で追加
+            title = re.sub('【|】|　', ' ', text)
+            title = convert_date(title) 
+            data.append([url, title])
     return data
+
+
+def convert_date(text):
+    # (8/14～8/16)のような形式をマッチさせる正規表現パターン
+    pattern = r'\((\d+)\/(\d+)～(\d+)\/(\d+)\)'
+    match = re.search(pattern, text)
+    if match:
+        start_month, start_day, end_month, end_day = match.groups()
+        start_date = datetime.strptime(f'{start_month}/{start_day}', '%m/%d')
+        end_date = datetime.strptime(f'{end_month}/{end_day}', '%m/%d')
+        # 変換後のテキストを生成
+        new_text = f'{start_date.month}月{start_date.day}日～{end_date.month}月{end_date.day}日'
+        text = text.replace(match.group(0), new_text)
+    return text
--- main.py
+++ main.py
@@ -13,8 +13,16 @@ def generate_flowchart(urls):
             if parent:
                 parent_to_find = nodes[parent]
                 child_to_find = nodes[node_id]
-                parent_title = [arr[1] for arr in urls if parent_to_find in arr][0]
-                child_title =  [arr[1] for arr in urls if child_to_find in arr][0]
+                parent_title = [arr[1] for arr in urls if parent_to_find in arr]
+                if parent_title:
+                    parent_title = parent_title[0]
+                else:
+                    parent_title = "Parentnotfound"
+                child_title =  [arr[1] for arr in urls if child_to_find in arr]
+                if child_title:
+                    child_title = child_title[0]
+                else:
+                    child_title = "Childnotfound"
                 links.append(f"{nodes[parent]+ '['+ parent_title  + ']'} --> {nodes[node_id]+ '['+ child_title + ']'}")
             parent = node_id
     # ノードを名前でソート
@@ -29,16 +37,20 @@ def generate_flowchart(urls):
 flowchart TB
 {link_md}
 ```"""
-urls = [['sample.com','title1'],
-['sample.com/a','title2'],
-['sample.com/b','title3'],
-['sample.com/c','title4'],
-['sample.com/d','title5'],
-['sample.com/a/aa','title6'],
-['sample.com/a/bb','title7'],
-['sample.com/a/cc','title8'],
-['sample.com/b/cc','title9'],]
+# urls = [['sample.com','title1'],
+# ['sample.com/a','title2'],
+# ['sample.com/b','title3'],
+# ['sample.com/c','title4'],
+# ['sample.com/d','title5'],
+# ['sample.com/a/aa','title6'],
+# ['sample.com/a/bb','title7'],
+# ['sample.com/a/cc','title8'],
+# ['sample.com/b/cc','title9'],]
 
 urls = convert_csv_to_array.read_csv()
+
 flowchart_md = generate_flowchart(urls)
 print(flowchart_md)
+f = open('./data/result.md', 'w')
+f.write(flowchart_md)
+f.close()
